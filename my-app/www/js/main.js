2b8add13afd4e9d5d31026caa9b4220b4550942a
Pim Verlangen <pim.verlangen@gmail.com>
2015-02-23 16:26:12 +0100

    nacho time!

--- my-app/www/js/main.js
+++ my-app/www/js/main.js
@@ -1,6 +1,249 @@
+	 moviesData = 
+	[
+		{
+			id: 1,
+			type: 'movie',
+			name: "movies-pim",
+			img: "pim-m-1.png",
+			description: "Pims first movie!"
+		}, 
+		{
+			id: 2,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 3,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 4,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "pim-m-1.png",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 5,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 6,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "pim-m-1.png",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 7,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 8,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "pim-m-1.png",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 9,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 10,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "pim-m-1.png",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 11,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "pim-m-1.png",
+			description: "Daniels first movie!"
+		},
+		{
+			id: 12,
+			type: 'movie',
+			name: "movies-daniel",
+			img: "daniel-m-1.jpg",
+			description: "Daniels first movie!"
+		},
+	];
+	
+	 showsData = 
+	[
+		{
+			id: 1,
+			type: 'tv-show',
+			name: "shows-pim",
+			img: "no-img.jpg",
+			description: "Pims first tv show!"
+		}, 
+		{
+			id: 2,
+			type: 'tv-show',
+			name: "shows-daniel",
+			img: "no-img.jpg",
+			description: "Daniels first tv show!"
+		}
+	];
+	
+	 cartoonsData = 
+	[
+		{
+			id: 1,
+			type: 'cartoon',
+			name: "cartoons-pim",
+			img: "no-img.jpg",
+			description: "Pims first cartoon!"
+		}, 
+		{
+			id: 2,
+			type: 'cartoon',
+			name: "cartoons-daniel",
+			img: "no-img.jpg",
+			description: "Daniels first cartoon!"
+		}
+	];
+
 $(document).ready(function () {
-    console.log($("#color-2").val());
-    $("#color-2").on("change", function () {
-        $("div[data-role='content']").css('background-color', $(this).val());
-    });
+	$('#item-list').bind('pagebeforeshow', function() {
+		$('.content-list').listview('refresh');
+	});
+
+	$('.type-container > a').on('click', function() {
+		//Fill item-list
+		loadListContent(this);
+		
+	});		
 });
+
+function loadListContent(anchor) {
+	var type = $(anchor).attr('data-type');
+	
+	var data = retrieveDataByType(type);
+
+	//Clear list
+	$('.content-list').empty();
+	
+	//Append data
+	$.each(data, function(index, element) {
+		var listItem = '<li><a id="' + element.id + '"  data-type="' + element.type + '" href="#detail-page"><img class="ui-li-thumb" src="img/' + element.img + '">' + element.name + '</a></li>';
+		$('.content-list').append(listItem);
+	});	
+	
+	attachListeners();	
+}
+
+function attachListeners() {
+	$('.content-list > li > a').on('click', function() {
+		var id = parseInt($(this).attr('id'));
+		var type = $(this).attr('data-type');
+		var itemDescription = $('#item-description');
+		var data = retrieveDataByType(type);
+		var object = $.grep(data, function(e){ return e.id == id; });
+		if (object) {
+			loadDetailPage(object[0]);
+		}
+	});
+}
+
+function loadDetailPage(item) {
+	$('#item-title').text(item.name);
+	$('#item-description').text(item.description);
+	
+	$('#item-img-container').empty();
+	$('#item-img-container').append('<img style="border-style: groove;" src="img/' + item.img + '" height="150" height="150">');
+}
+
+function checkScroll() {
+    /* You always need this in order to target
+       elements within active page */
+    var activePage = $.mobile.pageContainer.pagecontainer("getActivePage"),
+ 
+        /* Viewport's height */
+        screenHeight = $.mobile.getScreenHeight(),
+ 
+        /* Content div - include padding too! */
+        contentHeight = $(".ui-content", activePage).outerHeight(),
+ 
+        /* Height of scrolled content (invisible) */
+        scrolled = $(window).scrollTop(),
+ 
+        /* Height of both Header & Footer and whether they are fixed
+           If any of them is fixed, we will remove (1px)
+           If not, outer height is what we need */
+        header = $(".ui-header", activePage).outerHeight() - 1,
+        footer = $(".ui-footer", activePage).outerHeight() - 1,
+ 
+        /* Math 101 - Window's scrollTop should
+           match content minus viewport plus toolbars */
+        scrollEnd = contentHeight - screenHeight + header + footer;
+ 
+    /* if (pageX) is active and page's bottom is reached
+       load more elements  */
+    if (activePage[0].id == "item-list" && scrolled >= scrollEnd) {
+        /* run loadMore function */
+        addMore(activePage);
+    }
+}
+
+function addMore(page) {
+	$(document).off("scrollstop");
+	
+	$.mobile.loading("show" , {
+		text: "Loading more elements...",
+		textvisible: true
+	});
+	
+	setTimeout(function() {
+		var items = '';
+		var last = $("li" , page).length;
+		var cont = last + 5;
+		
+		for (var i = last; i < cont; i++) {
+			items += '<li><a id="' + (9000+i) + '"  data-type="unknown" href="#detail-page"><img class="ui-li-thumb" src="img/no-img.jpg">Element' + i + ' after scroll.</a></li>';
+		}
+		$(".content-list").append(items).listview('refresh');
+		$.mobile.loading("hide");
+		$(document).on("scrollstop", checkScroll);
+	}, 500);
+}
+
+function retrieveDataByType(type) {
+	var data;
+	switch(type) {
+		case 'movie':
+			data = moviesData;
+			break;
+		case 'tv-show':
+			data = showsData;
+			break;
+		case 'cartoon':
+			data = cartoonsData;
+			break;
+		default:
+			data = [];
+			break;
+	}
+	return data;
+}
+
+$(document).on("scrollstop", checkScroll);
+
